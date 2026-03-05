f7fa4967c0d13b23b58a476032ec60afb2517904
Amit Thakur <amitthakur70072@gmail.com>
2026-03-05 09:39:21 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+# a={"name": "amit",
+#    "class": 12}
+# b={"gender":"male"}
+# print(a["class"])
+# a["aman"]=11
+# a["name"]="python"
+# print(a)
+# a.pop("name")
+# print(a)
+# a.keys()
+
+# print(a.values())
+# print(a.items())
+# (a.update(b))
+# print(a)
+# a.update({"gender": "female"})
+# print(a)
+
+# a=int(input("enter number: "))
+# print(a+5)
+# a={1,2,3,4,5}
+# print(a.pop((4)))
+# print(a)
+
+
+
+
+
+# d = {'a': 1, 'a': 2} 
+# print(d)
