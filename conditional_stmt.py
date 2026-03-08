0e572d4e207293a63905bf2bf913d71cb599bed2
Amit Thakur <amitthakur70072@gmail.com>
2026-03-08 10:15:17 +0530

    Add files via upload

---
+++ conditional_stmt.py
@@ -0,0 +1,133 @@
+# name=input("enter your name: ")
+# time= int(input("enter the time: "))
+# if(time<12):
+#     print(f"Good morning! {name}")
+# elif(time==12):
+#     print(f"Good Afternoon {name}")
+# else:
+#     print("it's evening time")
+
+# a=int(input("enter a number:"))
+# if(a>=0):
+#     print("number is positive")
+# else:
+#     print("number is negative")
+# if(a>=50):
+#     print("number is greater")
+# if(a%2==0):
+#     print('number is even')
+# if(a>18):
+#     print("age is greater than 18")
+# if(a=="python"):
+#     print("hello")
+# else:
+#     print(a)
+# if(a<0) :
+#     print("invalid number")
+# elif(a>100):
+#     print("invalid")
+# elif(a>90  and a<100):
+#     print("a grade")
+# elif(a>75 and a<100):
+#     print("b grade")
+# elif(a>60 and a<100):
+#     print("c grade")
+# else:
+#     print("fail")
+# day=int(input("enter day number" ))
+# if(day==1):
+#     print("monday")
+# elif(day==2):
+#     print("tuesday")
+# elif(day==3):
+#     print("wednesday")
+# elif(day==4):
+#     print("thursday")
+# elif(day==5):
+#     print("friday")
+# elif(day==6):
+#     print("saturday")
+# elif(day==7):
+#     print("sunday")
+# else:
+#     print("invalid day entered")
+# temperature=int(input("enter temperature in degree : "))
+# if(temperature>30):
+#     print("hot day")
+# elif(temperature>=20 and temperature<=30):
+#     print("normal temperature")
+# elif(temperature<20):
+#     print("It's a cold day")
+# else:
+#     print("wrong entry")
+# number=int(input("enter first number: "))
+# number2=int(input("enter second number: "))
+# # number3=int(input("enter third number: "))
+# # if(number>number2 and number>number3):
+# #     print("first number is greater")
+# a=12
+# if(a>0): print("it is a positive number")
+# marks=int(input("enter the marks: "))
+# print("passed") if marks>24 else print("failed")
+
+# username=input("enter the username: ")
+# password=input("enter the password: ")
+# if(username=="admin" and password=="1234"):
+#     print("valid user")
+# else:
+#     print("invalid username and password")
+
+# year=int(input("enter the year: "))
+# if(year%4==0 and (year%400==0 and year%100!=0 ):
+#     print("leap year")
+# else:
+#     print("not a leap year")
+
+
+# year=int(input("enter any century year: "))
+# if(year%100==00):
+#     print("century yr")
+# else:
+#     print("casual year")
+
+# char=input("enter something you want to know: ")
+# if(char.isupper()):
+#     print("upper case ")
+# elif(char.isdigit()):
+#     print("it's a digit")
+# elif(char.isnumeric()):
+#     print("numeric")
+# elif(char.islower()):
+#     print("it is lower lettr") 
+# else:
+#     print("it's mixed")     
+
+#check the electritity bills based on units:
+# units_consumed= int(input("enter a number: "))
+# if(units_consumed<=100 and units_consumed>0):
+#     print(units_consumed*1.5)
+# elif(units_consumed>=101 and units_consumed<=200):
+#     print(units_consumed*2.5)
+# elif(units_consumed>=201 and units_consumed<=300):
+#     print(units_consumed*4)
+# elif(units_consumed>=301):
+#     print(units_consumed*6)
+# else:
+#     print("invalid entry")
+
+# #calculator
+# a=float(input("enter a number: "))
+# b=float(input("enter a number: "))
+# op=input("enter any operator(+,-,*,/,%): ")
+# if(op=="+"):
+#     print("result= ",a+b)
+# elif(op=="*"):
+#     print("result: ",a*b)
+# elif(op=="/"):
+#     print("result: ",a/b)
+# elif(op=="%"):
+#     print("result: ",a%b)
+# elif(op=="-"):
+#     print("result: ",a-b)
+# else:
+#     print("invalid operator! try again baby!:)")
