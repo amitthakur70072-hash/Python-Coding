62cc09f79a37588a760931d097291c2962f3abcb
Amit Thakur <amitthakur70072@gmail.com>
2026-03-09 09:49:41 +0530

    Add files via upload

---
+++ practice_cond stmts.py
@@ -0,0 +1,32 @@
+# i=0
+# while(i<=10):
+#  print(i)
+# i=i+1
+
+#2 i=10
+# while(i>0):
+#     print(i)
+#     i-=1
+
+# i=0
+# while( i<=20):
+#  print(i)
+#  i+=2
+# i=1
+# while(i<=20):
+#     print(i)
+#     i+=2
+# i=0
+# while(i<=10):
+#     print("7 *",i,"=",7*i)
+#     i+=1
+# n = int(input("Enter a number (0 to stop): "))
+# while n != 0:
+#     print(n)
+#     n = int(input("Enter a number (0 to stop): "))...........................best question
+
+n=int(input("enter a number: "))
+i=0
+total=0
+while(i<=n):
+    print()
