89faedd1c8b254219394d57f9c1fae0d43529866
Amit Thakur <amitthakur70072@gmail.com>
2026-03-14 16:18:49 +0530

    Add files via upload

---
+++ first order func.py
@@ -0,0 +1,33 @@
+# Create a function double(x) and a higher order function apply(func, value) that applies the function to the value.
+# def double(x):
+#     return x*x
+# def high(func,value):
+#     return func(value)
+# r= high(double,5)
+# print(r)
+
+# function that returns the square of a number
+# def square(n):
+#     return n*n
+# x=square(3)
+# print(x)
+
+# Write is_even(n) that returns True if even, else False.
+# def is_even(n):
+#     if(n%2==0):
+#         return True
+#     else:
+#         return False
+# n=int(input("enter the number: "))
+# X=is_even(n)
+# print(X)
+
+# Write a function greet(name) that prints:
+def greet(name):
+    print(f"hello {name}")
+na=input("enter the name: ")
+greet(na)
+
+
+
+# add a list to the function
