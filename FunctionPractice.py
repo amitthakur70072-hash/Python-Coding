ebaf20e2492d957de7d63987b0ff298b621a4160
Amit Thakur <amitthakur70072@gmail.com>
2026-03-15 10:12:12 +0530

    Add files via upload

---
+++ FunctionPractice.py
@@ -0,0 +1,64 @@
+# def nit(x):
+#     print(x)
+# nit("hello world")
+
+# def sq(a):
+#     print(a*a)
+# sq(5)
+# sq(4)
+    
+# def function(a,b):
+#     print(a+b)
+# function(12,23)
+
+# def even(a):
+#     if(a%2==0):
+#         print("a is a even number")
+#     else:
+#         print("odd number")
+# even(12)
+
+# # Write a function to find the largest of two numbers
+# def find(a,b):
+#     if(a>b):
+#         print("a is greater")
+#     elif(b>a):
+#         print("b is greater")
+#     else:
+#         print("both are equal")
+# find(12,34)
+
+# Write a function that takes a name as input and prints a greeting message
+# n= input("enter your name: ")
+# def name(n):
+#     print(f"hello {n}")
+# name(n)
+
+# Write a function to find the cube of a number
+# n=int(input("enter a number: "))
+# def cube(n):
+#     print(f"cube of entered number is :{n*n*n}")
+# cube(n)
+
+# Write a function to calculate the area of a rectangle.
+# b=int(input("enter a number(base): "))
+# h=int(input("enter a number(height): "))
+# def area(b,h):
+#     rectangeA=(b*h)/2
+#     print(f"area of rectangle is {rectangeA}")
+# area(b,h)
+
+# Write a function that returns the length of a string
+# n= input("enter your name: ")
+# def le(n):
+#    return len(n)
+# print(f"the length of the string entered is {len(n)} ")
+
+# Write a function to convert Celsius to Fahrenhei:(0°C × 9/5) + 32 
+# i= int(input("enter the temperature:"))
+# def temp():
+#     temp1=(i*9/5) + 32 
+#     print(temp1)
+# temp()
+
+
