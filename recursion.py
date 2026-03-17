8199e325e49bbfedf44a2e1a39e3a167287f6d75
Amit Thakur <amitthakur70072@gmail.com>
2026-03-17 10:11:38 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,39 @@
+# A function that recalls itself
+# def factorial(n):
+#     if(n==1 and n==0):
+#         return 1
+#     return n* factorial(n-1)
+# print(factorial(5)+2)
+# print(factorial(5)*3)
+
+# find fact of 100
+# def factorial(n):
+#     if n==1:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+# print(factorial(8))
+
+
+# print * pattern A
+# for  i in range(1,6):
+#     print("*", end=" ")
+#     print(" ", end=" ")
+#     print(" ", end=" ")
+#     print("*")
+
+# find the greatest of three numbers:
+# def greatest (a,b,c):
+#     if(a>=b and a>= c):
+#         return a
+#     elif(b>a and b>c):
+#         return b
+#     elif(c>a and c>b):
+#         return c 
+#     else:
+#         print("invalid statements")
+# a= int(input(" 1st number: "))
+# b= int (input("2nd number: "))
+# c=int(input("3rd number: "))
+# x= greatest(a,b,c)
+# print(x)
