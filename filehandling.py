4d7e6c2a7856ba31ccc4e71823ff1d2e26cf8d6b
Amit Thakur <amitthakur70072@gmail.com>
2026-03-24 09:50:22 +0530

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,15 @@
+# f=open("file.txt","r")
+# data=f.read()
+# print(data)
+# print(type(data))
+# f.close()
+
+
+# f=open("myfile.txt","r")
+# data=f.read()
+
+# print(data)
+# print(type(data))
+# fp=f.readline()
+# print(fp)
+# f.close()
